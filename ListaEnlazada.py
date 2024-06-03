db5c9429c516e0164b3c4bb6a7d50daf6a86a1db
Matias Daniel Alessandrello <135188526+malessan4@users.noreply.github.com>
2024-06-02 21:26:09 -0300

    Lista Enlazada TDA
    Lista Enlazada TDA

--- ListaEnlazada.py
+++ ListaEnlazada.py
@@ -19,16 +19,26 @@ class ListaEnlazada:
             
         self.len += 1
         
+# si el primer elemento es distino de None, crea una variable que va a ir almacenando las representaciones de la lista        
+# y usa una variabe llamada nodo que es la que va ir recorriendo cada uno los nodos creados en la lista   
+# se guarda el primero y a la lista le agrego el dato del primer nodo
+# despues se pregunta, tengo siguiente? 
+# si tiene siguiente me paso al sigiuente nodo, le agrego un espacio, le inserto el dato del nuevo nodo y pregunta nuevamente  
+# si no tiene siguiente se cierra la lista con un corchete  
+                
     def __str__(self):
-        if self.primero is None:
+        if self.primero is None: 
             return "[]"
         else:
-            str_lista = "[ "
+            str_lista = "[ " #abrimos un corchete para la lista
             nodo = self.primero
             str_lista += str(self.primero.dato)
             while(nodo.next is not None):
-                nodo = nodo.next
-                str_lista += 1
+                nodo = nodo.next # el nodo pasa a ser el siguiente
+                str_lista += " " # le agregamos un espasio al string de la lista
+                str_lista += str(nodo.dato) # le insertamos al string de la lista del dato del nodo
+            str_lista += " ]" # se cierra el corchete de la lista
+            return str_lista
 
             
             
