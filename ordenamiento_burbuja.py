4d04eaaf3dc3e4296c5dbe712cc45f40d7335370
Matias Daniel Alessandrello <135188526+malessan4@users.noreply.github.com>
2024-06-03 08:55:57 -0300

    ordenamiento_burbuja e insercion
    ordenamiento_burbuja e insercion

---
+++ ordenamiento_burbuja.py
@@ -0,0 +1,32 @@
+"""
+lista1 = [5, 3, 1 , -2, 8, 4, 0, -6]
+
+lista1.sort()
+print(lista1)
+
+lista1.sort(reverse=True)
+print(lista1)
+
+
+lista1 = [5, 3, 1 , -2, 8, 4, 0, -6]
+
+indice = 0
+print(lista1[indice])
+
+lista1[len(lista1)-1]
+
+for i in range(0, len(lista1)):
+    print(i, lista1[i])
+"""
+    
+lista2 = [8, 5, -1, 6, 3, 4, -6, 0]
+
+
+# Ordenamiento burbuja
+for j in range(len(lista2)-1, 0, -1):
+    for i in range(0, j):
+        if lista2[i] < lista2[i+1]:
+            lista2[i], lista2[i+1] = lista2[i+1], lista2[i]
+
+print(f"pasada {j} la lista quedo {lista2}")
+
---
+++ ordenamiento_insercion.py
@@ -0,0 +1,21 @@
+
+# Ordenamiento por insercion
+def ordenamineto_por_insercion(lista_desordenada):
+    for i in range(1, len(lista_desordenada)):
+        j = i-1
+        siguiente_elemento = lista_desordenada[i]
+        
+        while j >= 0 and lista_desordenada[j] > siguiente_elemento: # cambiando el simbolo > por < se elije de forma ascendente o descendente
+            lista_desordenada[j + 1] = lista_desordenada[j]
+            j -= 1
+        
+        lista_desordenada[j+1] = siguiente_elemento
+        
+    return lista_desordenada
+
+
+lista3 = [-6, 5, 3, 1, 8, 7, 11, 0]
+
+ordenamineto_por_insercion(lista3)
+
+print(lista3)
